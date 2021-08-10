1a4a67566690d9f072d906e73175031dd35d5c8c
teamy <teamy@DESKTOP-TMDNQ07>
2021-08-10 07:34:39 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace VMBR_AutoUpdater
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.versionLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(121, 53);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "download latest release";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// versionLabel
+			// 
+			this.versionLabel.AutoSize = true;
+			this.versionLabel.Location = new System.Drawing.Point(9, 68);
+			this.versionLabel.Name = "versionLabel";
+			this.versionLabel.Size = new System.Drawing.Size(134, 17);
+			this.versionLabel.TabIndex = 1;
+			this.versionLabel.Text = "Cannot find version.";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(587, 245);
+			this.Controls.Add(this.versionLabel);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "VM:BR - Auto Updater";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label versionLabel;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace VMBR_AutoUpdater
+{
+	public partial class Form1 : Form
+	{
+
+		public class jsona
+		{
+			public string version { get; set; }
+		}
+
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			using (var client = new WebClient())
+			{
+				string homeFolderVMBR = @"C:\Users\"+Environment.UserName+@"\VMBR";
+				string downloadToThisPath = @"C:\Users\" + Environment.UserName + @"\VMBR\AutoInstaller\"; //path
+				string jsonPath = @"C:\Users\" + Environment.UserName + @"\VMBR\AutoInstaller\version.json"; // peth
+				// If directory does not exist, create it. 
+				if (!Directory.Exists(homeFolderVMBR))
+				{
+					Directory.CreateDirectory(homeFolderVMBR);
+					Directory.CreateDirectory(downloadToThisPath);
+				}
+				WebClient webClient = new WebClient();
+				//webClient.DownloadFile("http://raw.githubusercontent.com/MichaelEpicA/VMBattleRoyale/development/version.json", downloadToThisPath+@"version.json");
+				string content = System.IO.File.ReadAllText(jsonPath);
+				var version = JsonConvert.DeserializeObject<List<jsona>>(jsonPath);
+				version.Select(appVer => appVer.version.Equals("version")); // painnnnn and suferinnnnngngggggggg
+				versionLabel.Text = version.Select(appVer => appVer.version.Equals("version"));
+			}
+
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
